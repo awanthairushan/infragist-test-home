18f7d631ccdc6a785fa749e4e11afc51fadc9997
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2023-07-12 10:10:53 +0530

    created the layout

---
+++ src/components/home/banner-section/BannerSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BannerSection = () => {
+    return (
+        <div>
+            This is banner section
+        </div>
+    );
+};
+
+export default BannerSection;
